2f964a6d61f97a6e8dd692f5e0350f4e771a12c0
TERRY WONG <iamtktwong@gmail.com>
2021-09-22 19:04:27 +0800

    add TickTock test case

---
+++ src/main/java/com/appswalker/model/TickTock.java
@@ -0,0 +1,24 @@
+package com.appswalker.model;
+
+public class TickTock {
+    private String tick;
+    private String tock;
+
+    public TickTock(String tick, String tock) {
+        this.tick = tick;
+        this.tock = tock;
+    }
+
+    public String getTick() {
+        return tick;
+    }
+
+    public String getTock() {
+        return tock;
+    }
+
+    @Override
+    public String toString() {
+        return "TickTock{" + "tick=" + tick + ", tock=" + tock + '}';
+    }
+}
---
+++ src/main/java/com/appswalker/services/TickTockStarter.java
@@ -0,0 +1,48 @@
+package com.appswalker.services;
+
+import com.appswalker.model.TickTock;
+import com.appswalker.model.TickTockQualifier;
+
+import java.util.Date;
+import java.util.Random;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.TimerService;
+import javax.enterprise.concurrent.ManagedExecutorService;
+import javax.enterprise.event.Event;
+import javax.enterprise.event.NotificationOptions;
+import javax.inject.Inject;
+
+@Singleton
+@Startup
+public class TickTockStarter {
+    @Resource
+    private TimerService tsvc;
+
+    @PostConstruct
+    public void init() {
+        tsvc.createTimer(10000, 5000, null);
+        System.out.println("timer created......");
+    }
+
+    @Inject
+    @TickTockQualifier
+    private Event<TickTock> eventBus;
+
+
+    private static final Random gen = new Random();
+
+    @Resource
+    private ManagedExecutorService mes;
+
+    @Timeout
+    public void onTrigger() {
+        System.out.println("timer triggered");
+        eventBus.fireAsync(new TickTock("tick-"+gen.nextInt(10), "tock-"+gen.nextInt(10)),
+                NotificationOptions.builder().setExecutor(mes).build());
+        System.out.println("Fired CDI event from thread "+ Thread.currentThread().getName());
+    }
+}
